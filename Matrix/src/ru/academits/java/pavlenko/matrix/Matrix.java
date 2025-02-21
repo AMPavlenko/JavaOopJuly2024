237cb0c7f3e57331561cd11ff82ae9f66f348da8
AMPavlenko <pavlyenko@gmail.com>
2025-02-21 21:17:59 +0700

    Commit 14

--- Matrix/src/ru/academits/java/pavlenko/matrix/Matrix.java
+++ Matrix/src/ru/academits/java/pavlenko/matrix/Matrix.java
@@ -19,7 +19,6 @@ public class Matrix {
         for (int i = 0; i < n; i++) {
             this.rows[i] = new Vector(m);
         }
-
     }
 
     public Matrix(Matrix matrix) {
@@ -44,5 +43,99 @@ public class Matrix {
         for (int i = 0; i < n; i++) {
             this.rows[i] = new Vector(matrixComponents[i]);
         }
+    }
+
+    public Matrix(Vector[] vectors) {
+        if (vectors.length == 0 || vectors[0].getSize() == 0) {
+            throw new IllegalArgumentException("Matrix dimensions must be positive");
+        }
+
+        this.n = vectors.length;
+        this.m = vectors[0].getSize();
+        this.rows = new Vector[n];
+
+        for (int i = 0; i < n; i++) {
+            if (vectors[i].getSize() != m) {
+                throw new IllegalArgumentException("All rows must have the same size");
+            }
+
+            this.rows[i] = new Vector(vectors[i]);
+        }
+    }
+
+    public int[] getSize() {
+        return new int[]{this.n, this.m};
+    }
+
+    public Vector getRow(int index) {
+        if (index < 0 || index >= this.n) {
+            throw new IndexOutOfBoundsException("Row index is out of bounds");
+        }
+
+        return new Vector(this.rows[index]);
+    }
+
+    public void setRow(int index, Vector vector) {
+        if (index < 0 || index >= this.n) {
+            throw new IndexOutOfBoundsException("Row index is out of bounds");
+        }
+
+        if (vector.getSize() != m) {
+            throw new IllegalArgumentException("Row size must match matrix column size");
+        }
+
+        rows[index] = new Vector(vector);
+    }
+
+    public Vector getColumn(int index) {
+        if (index < 0 || index >= m) {
+            throw new IndexOutOfBoundsException("Column index is out of bounds");
+        }
+
+        double[] columnVector = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            columnVector[i] = rows[i].getVectorComponent(index);
+        }
+
+        return new Vector(columnVector);
+    }
+
+    public Matrix matrixTranspose() {
+        Matrix transposedMatrix = new Matrix(m, n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                transposedMatrix.rows[j].setVectorComponent(i, rows[i].getVectorComponent(j));
+            }
+        }
+
+        return transposedMatrix;
+    }
+
+    public Matrix scalarMatrixMultiplication(double scalar) {
+        Matrix multiplicationResult = new Matrix(n, m);
+
+        for (int i = 0; i < n; i++) {
+            multiplicationResult.rows[i] = rows[i].scalarVectorMultiplication(scalar);
+        }
+
+        return multiplicationResult;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+
+        for (int i = 0; i < rows.length; i++) {
+            sb.append(rows[i].toString());
+
+            if (i < rows.length - 1) {
+                sb.append(", ");
+            }
+        }
+
+        sb.append("}");
+        return sb.toString();
     }
 }

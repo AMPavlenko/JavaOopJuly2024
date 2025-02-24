15ff8e1dff2a90bcb4fb0bc22d5369e5d6019423
AMPavlenko <pavlyenko@gmail.com>
2025-02-24 22:07:20 +0700

    Commit 14

--- Matrix/src/ru/academits/java/pavlenko/matrix/Matrix.java
+++ Matrix/src/ru/academits/java/pavlenko/matrix/Matrix.java
@@ -128,18 +128,44 @@ public class Matrix {
             throw new UnsupportedOperationException("Детерминант может быть посчитан только для квадратной матрицы.");
         }
 
-        if (this.n == 2) {
-            return this.rows[0].getVectorComponent(0) * this.rows[1].getVectorComponent(1) -
-                    this.rows[0].getVectorComponent(1) * this.rows[1].getVectorComponent(0);
+        return determinantHelper(this.rows);
+    }
+
+    private double determinantHelper(Vector[] rows) {
+
+        if (rows.length == 2) {
+            return rows[0].getVectorComponent(0) * rows[1].getVectorComponent(1) -
+                    rows[0].getVectorComponent(1) * rows[1].getVectorComponent(0);
         }
 
         double det = 0;
 
-        for (int i = 0; i < this.m; i++) {
-            det += (i % 2 == 0 ? 1 : -1) * rows[0].getVectorComponent(i);
+        for (int i = 0; i < rows[0].getSize(); i++) {
+            det += (i % 2 == 0 ? 1 : -1) * rows[0].getVectorComponent(i) *
+                    determinantHelper(getMinor(rows, 0, i));
         }
 
         return det;
+    }
+
+    private Vector[] getMinor(Vector[] rows, int row, int col) {
+        Vector[] minor = new Vector[rows.length - 1];
+
+        for (int i = 0; i < rows.length; i++) {
+            if (i == row) continue;
+
+            double[] newRow = new double[rows[i].getSize() - 1];
+
+            for (int j = 0; j < rows[i].getSize(); j++) {
+                if (j == col) continue;
+
+                newRow[j > col ? j - 1 : j] = rows[i].getVectorComponent(j);
+            }
+
+            minor[i > row ? i - 1 : i] = new Vector(newRow);
+        }
+
+        return minor;
     }
 
     public String toString() {
--- Matrix/src/ru/academits/java/pavlenko/matrix/main/Main.java
+++ Matrix/src/ru/academits/java/pavlenko/matrix/main/Main.java
@@ -1,8 +1,31 @@
 package ru.academits.java.pavlenko.matrix.main;
 
 
+import ru.academits.java.pavlenko.matrix.Matrix;
+import ru.academits.pavlenko.vector.Vector;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Создание матрицы нулей размера nxm
+        Matrix matrix1 = new Matrix(3, 3);
+
+        // Проверка конструктора копирования
+        Matrix matrix2 = new Matrix(matrix1);
+
+        // Проверка конструктора для создания матрицы из двумерного массива
+        double[][] range = {{1, 3, 5}, {5, 1, 5}, {3, 3, 2}};
+        Matrix matrix3 = new Matrix(range);
+
+        // Проверка конструктора для создания матрицы из массива векторов строк
+        Vector[] vectors = {new Vector(new double[]{1, 3, 4}), new Vector(new double[]{3, 3, 8}), new Vector(new double[]{7, 2, 7})};
+        Matrix matrix4 = new Matrix(vectors);
+
+        // Проверка метода получения размера матрицы
+        System.out.printf("%nРазмер матрицы %s равен = %s.", matrix4, Arrays.toString(matrix4.getSize()));
+
+
+
     }
 }
